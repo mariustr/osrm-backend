3986653661f6eab9c6057a1aed7c3e0b8029b94b
Moritz Kobitzsch <moritz@mapbox.com>
2016-10-25 22:44:13 +0200

    dedicated extractor policy

--- include/util/geojson_debug_policies.hpp
+++ include/util/geojson_debug_policies.hpp
@@ -9,10 +9,6 @@
 #include "util/node_based_graph.hpp"
 #include "util/typedefs.hpp"
 
-#include "extractor/guidance/coordinate_extractor.hpp"
-#include "extractor/guidance/intersection.hpp"
-#include "extractor/guidance/toolkit.hpp"
-
 namespace osrm
 {
 namespace util
@@ -48,23 +44,6 @@ struct CoordinateVectorToMultiPoint
 {
     // converts a vector of node ids into a linestring geojson feature
     util::json::Object operator()(const std::vector<util::Coordinate> &coordinates) const;
-};
-
-// generate a visualisation of an intersection, printing the coordinates used for angle calculation
-struct IntersectionPrinter
-{
-    IntersectionPrinter(const util::NodeBasedDynamicGraph &node_based_graph,
-                        const std::vector<extractor::QueryNode> &node_coordinates,
-                        const extractor::guidance::CoordinateExtractor &coordinate_extractor);
-
-    // renders the used coordinate locations for all entries/as well as the resulting
-    // intersection-classification
-    util::json::Array operator()(const NodeID intersection_node,
-                                  const extractor::guidance::Intersection &intersection) const;
-
-    const util::NodeBasedDynamicGraph &node_based_graph;
-    const std::vector<extractor::QueryNode> &node_coordinates;
-    const extractor::guidance::CoordinateExtractor &coordinate_extractor;
 };
 
 } /* namespace util */
